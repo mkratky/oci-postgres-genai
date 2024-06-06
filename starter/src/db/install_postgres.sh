447b3973d7b095d7c679df4e39d12916277edb58
Marc Gueury <marc.gueury@oracle.com>
2024-06-06 21:08:33 +0200

    v1

--- starter/src/db/install_postgres.sh
+++ starter/src/db/install_postgres.sh
@@ -2,28 +2,26 @@
 # To run with root
 
 dnf module list postgresql
-dnf module switch-to postgresql:16
-dns install postgresql postgresql-server -y
-dnf install postgresql-server-devel -y
+dnf module switch-to postgresql:16 -y
+dnf install postgresql postgresql-server -y
+dnf install postgresql-server-devel -y --allowerasing
 
 # Configure it 
 postgresql-setup initdb
-ls /etc/system.d
 systemctl enable postgresql
 systemctl start postgresql
 systemctl status postgresql
 
 # Change the posgress user password
 su - postgres <<EOF
-psql -c "alter user postgres with password "${DB_PASSWORD}"
+psql -c "alter user postgres with password '${DB_PASSWORD}'"
 EOF
 
 # Change the indentification method from Indent to password
 if [ ! -f /var/lib/pgsql/data/pg_hba.conf.orig ]; then
   cp /var/lib/pgsql/data/pg_hba.conf /var/lib/pgsql/data/pg_hba.conf.orig
 fi
-sed -i "s/Indent/password/" /var/lib/pgsql/data/pg_hba.conf
-systemctl restart postgresql
+sed -i "sg/ident/password/g" /var/lib/pgsql/data/pg_hba.conf
 
 firewall-cmd --zone=public --add-port=5432/tcp --permanent
 firewall-cmd --reload
@@ -35,3 +33,5 @@ git clone --branch v0.7.1 https://github.com/pgvector/pgvector.git
 cd pgvector
 make
 make install 
+
+systemctl restart postgresql
