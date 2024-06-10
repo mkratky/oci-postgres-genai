7561334eb0bfafa80116d5b3430c44b62474cb51
Marc Gueury <marc.gueury@oracle.com>
2024-06-10 14:28:45 +0200

    v1

--- starter/src/after_done.sh
+++ starter/src/after_done.sh
@@ -36,5 +36,10 @@ cp -r src/compute/* $TARGET_DIR/compute/.
 scp -r -o StrictHostKeyChecking=no -i $TF_VAR_ssh_private_path $TARGET_DIR/compute/* opc@$BASTION_IP:/home/opc/.
 ssh -o StrictHostKeyChecking=no -i $TF_VAR_ssh_private_path opc@$BASTION_IP "export TF_VAR_language=python;export FN_INVOKE_ENDPOINT=\"$FN_INVOKE_ENDPOINT\";export FN_OCID=\"$FN_OCID\";export STREAM_MESSAGE_ENDPOINT=\"$STREAM_MESSAGE_ENDPOINT\";export STREAM_OCID=\"$STREAM_OCID\";export DB_USER=\"$TF_VAR_db_user\";export DB_PASSWORD=\"$TF_VAR_db_password\";export DB_URL=\"$DB_URL\"; bash compute_bootstrap.sh 2>&1 | tee -a compute_bootstrap.log"
 
-# XXXXXX Manual step ? XXXXXXXX
 oci os object bulk-upload -bn psql-public-bucket -ns $TF_VAR_namespace --overwrite --src-dir ../sample_files
+
+echo
+echo "-- SEARCH_URL -------"
+echo http://$BASTION_IP/"
+echo
+echo "Please wait 5 mins. The indexing of the sample files has just started." 
