4a9694355ed0c32511283d1a0e754d290885f551
Marc Gueury <marc.gueury@oracle.com>
2024-06-27 10:34:42 +0200

    v1

--- oracle/convert.sh
+++ oracle/convert.sh
@@ -5,12 +5,11 @@ cd $SCRIPT_DIR/..
 rm starter/src/db/*
 rm starter/src/terraform/psql.tf
 cp -r oracle/* starter/.
+
 sed -i 's/TF_VAR_db_type="psql"/TF_VAR_db_type="database"/' starter/env.sh
 sed -i 's/TF_VAR_prefix="psql"/TF_VAR_prefix="db23ai"/' starter/env.sh
 sed -i 's/POSTGRES/DB23ai/' starter/src/compute/app/requirements.txt
 sed -i 's/psycopg2-binary/oracledb/' starter/src/compute/app/requirements.txt
-
-cd 
 sed -i '/searchActionJS.js/a \
 sed -i "s/PostgreSQL/Oracle DB 23ai/" webApps/search_ui/flows/simple/pages/input-search-page.html \
 ' starter/src/ui/build_ui.sh
