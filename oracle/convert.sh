a5b8da1b9a4ddc15c0c94968360211777e48a436
Marc Gueury <marc.gueury@oracle.com>
2024-06-27 10:08:38 +0200

    v1

--- oracle/convert.sh
+++ oracle/convert.sh
@@ -9,4 +9,7 @@ sed -i 's/TF_VAR_db_type="psql"/TF_VAR_db_type="database"/' starter/env.sh
 sed -i 's/POSTGRES/DB23ai/' starter/src/compute/app/requirements.txt
 sed -i 's/psycopg2-binary/oracledb/' starter/src/compute/app/requirements.txt
 
-psycopg2-binary
+cd 
+sed -i '/searchActionJS.js/a \
+sed -i "s/PostgreSQL/Oracle DB 23ai/" webApps/search_ui/flows/simple/pages/input-search-page.html \
+' starter/src/ui/build_ui.sh
