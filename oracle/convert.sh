c940381e21caeb7bbcba8b47c92f583708d7cea3
Marc Gueury <marc.gueury@oracle.com>
2024-07-23 14:47:49 +0200

    v1

--- oracle/convert.sh
+++ oracle/convert.sh
@@ -2,7 +2,7 @@
 SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 cd $SCRIPT_DIR/..
 
-rm starter/src/db/*
+rm -r starter/src/db/*
 rm starter/src/terraform/psql.tf
 cp -r oracle/* starter/.
 
