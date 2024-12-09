6cb5bd1fa43166a0e0a6264cf77020d7b231658d
Marc Gueury <marc.gueury@oracle.com>
2024-12-09 17:53:45 +0100

    v1

--- agent/convert.sh
+++ agent/convert.sh
@@ -2,12 +2,6 @@
 SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 cd $SCRIPT_DIR/..
 
-if [ "$1" = "" ];
-  echo "ERROR: give TF_VAR_genai_agent_datasource_ocid value as parameter"
-  exit
-fi
-
 oracle/convert.sh
 cp -r agent/* starter/.
 
-echo "export TF_VAR_genai_agent_datasource_ocid=$1" > starter/src/compute/app/ingest.sh
