ed67a50b3c38ab44b278e6f2ed70f3db19fb1dc4
Marc Gueury <marc.gueury@oracle.com>
2024-12-10 11:08:06 +0100

    v1

--- agent/convert.sh
+++ agent/convert.sh
@@ -8,9 +8,9 @@ cp -r agent/* starter/.
 OLD_DIR=starter/src/terraform/old
 mkdir $OLD_DIR
 mv starter/src/terraform/network.tf $OLD_DIR
-rm starter/src/terraform/atp.tf $OLD_DIR
-rm starter/src/terraform/output.tf $OLD_DIR
-rm starter/src/terraform/apigw_atp.tf $OLD_DIR
+mv starter/src/terraform/atp.tf $OLD_DIR
+mv starter/src/terraform/output.tf $OLD_DIR
+mv starter/src/terraform/apigw_atp.tf $OLD_DIR
 
 sed -i 's/TF_VAR_prefix="psql"/TF_VAR_prefix="agent"/' starter/env.sh
 sed -i 's/TF_VAR_prefix="db23ai"/TF_VAR_prefix="agent"/' starter/env.sh
